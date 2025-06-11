1dfe4f4f8c6c954d52dda3f8d88f0090f62a1e24
sudharani <sudharaniburra6@gmail.com>
2025-06-11 08:02:09 +0530

    Add files via upload

---
+++ java/First.java
@@ -0,0 +1,12 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println(
+            "\t-----------------------------\n" +
+            "\t| S.No | Name   | Roll No  |\n" +
+            "\t|---------------------------|\n" +
+            "\t|                         |\n" +
+            "\t|                         |\n" +
+            "\t-----------------------------\n"
+        );
+    }
+}
---
+++ java/Second.java
@@ -0,0 +1,14 @@
+public class Second{
+    public static void main(String[] args) {
+        System.out.println(" Project");
+        System.out.println("----------------------------------------");
+        System.out.println("This is Kishor; from 'CSE' section.");
+        System.out.println("I am doing one project \"Traffic control using IOT\"");
+        System.out.println("My Tear\bms members are:\n");
+
+        System.out.println("S.No\tName\t\tRollNo");
+        System.out.println("1\tsudha\t\t501");
+        System.out.println("2\tlik\t\t502");
+        System.out.println("3\tmummy\t\t503");
+    }
+}
